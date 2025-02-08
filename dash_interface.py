a432f06565e4bf6eb6125ab717ef7a8fe6ef0e99
david-petrovic <david.petrovic@akane.waseda.jp>
2025-02-08 15:42:08 +0100

    feat: Include callback function into dash file

--- dash_interface.py
+++ dash_interface.py
@@ -1,3 +1,6 @@
+# Next step, watch and understand this video: https://www.youtube.com/watch?v=pNMWbY0AUJ0&t=284s
+
+
 # Source Link for further steps: https://github.com/Coding-with-Adam/Dash-by-Plotly/blob/master/Other/Dash_Introduction/intro.py
 
 # Notes, include a FAQ at the end of the page where you answer questions such as: 1.) Why does the year only include 252 days?
@@ -6,7 +9,12 @@
 import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
-from dash import Dash, dcc, html, Input, Output
+from dash import Dash, dcc, html, Input, Output, callback
+
+import life_expectancy as lf
+import stock_movements as sm
+
+country_options = lf.get_countries()
 
 
 app = Dash(__name__)
@@ -48,10 +56,10 @@ app.layout = html.Div([
             html.Label(["Country:"], style={'font-weight' : 'bold', 'text-align' : 'center'}),
                     dcc.Dropdown(
                         id="country",
-                        options=[x for x in range(1, 120)],
-                        value=62,
+                        options=country_options,
+                        value="Switzerland",
                         clearable=False,
-                        searchable=False,
+                        searchable=True,
                     ),
         ], style=dict(width='33.33%')),
         
@@ -63,7 +71,7 @@ app.layout = html.Div([
 
 
     
-
+    dcc.Markdown(children='', id='displayed_text'),
 
     html.Div(id='output_container', children=[]),
 
@@ -73,6 +81,15 @@ app.layout = html.Div([
 
 ])
 
+@callback(
+    Output('displayed_text', 'children'),
+    Input('age', component_property='value'),
+    Input('sex', component_property='value'),
+    Input('country', component_property='value')
+)
+
+def update_text(age, sex, country):
+    return f"The values chosen by the user are: age: {age}, sex: {sex}, country: {country}"
 
 
 
